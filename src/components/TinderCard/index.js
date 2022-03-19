1554cf1af7850725df6c2c6a30ea6f0015e8536b
NatC02 <NatanCeballos02@gmail.com>
2022-03-19 13:31:09 -0400

    custom tinder card component

---
+++ src/components/TinderCard/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import Card from './src/components/TinderCard';
+import users from './assets/data/users';
+
+const App = () => {
+  return (
+    <View style={styles.pageContainer}>
+      <Card user={users[2]} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
+
+export default App;
